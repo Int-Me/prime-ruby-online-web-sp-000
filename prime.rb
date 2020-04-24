1758f55792c1a7f930c0dd377fe9def054f32b09
Roland McRae <mcraeroland@gmail.com>
2020-04-24 09:09:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(number)
-  for (i = 2, i < number, i++)
+  for (i = 2, i < number)
     if
       n % i == 0
       return false
     else
       true
     end
+    i++
   end
 end
